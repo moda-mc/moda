c755597302c1f77777efc4e3a9c2bc10ba6c3bcd
PatchPC <42698687+patchtv@users.noreply.github.com>
2019-06-15 18:53:09 +0200

    added color parsing to voteevent broadcast

--- src/main/java/com/mineglade/icore/events/VoteEvent.java
+++ src/main/java/com/mineglade/icore/events/VoteEvent.java
@@ -5,6 +5,7 @@ import com.mineglade.icore.PrefixType;
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -16,7 +17,7 @@ public class VoteEvent implements Listener {
         Vote vote = event.getVote();
 
         ICore.economy.bankDeposit(vote.getUsername(), 25);
-        Bukkit.broadcastMessage(ICore.getPrefix(PrefixType.COMMAND) + vote.getUsername() + " has voted on " + vote.getServiceName() + "!");
+        Bukkit.broadcastMessage(ICore.getPrefix(PrefixType.COMMAND) + ChatColor.translateAlternateColorCodes('&', vote.getUsername() + " has voted on " + vote.getServiceName() + "!"));
 
     }
 
