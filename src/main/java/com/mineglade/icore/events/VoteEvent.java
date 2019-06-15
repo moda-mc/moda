4aeb01a72a1c5511f36179fd5bf1dda8194f69c0
PatchPC <42698687+patchtv@users.noreply.github.com>
2019-06-15 17:04:32 +0200

    added votelistener
    & votecommand

---
+++ src/main/java/com/mineglade/icore/events/VoteEvent.java
@@ -0,0 +1,23 @@
+package com.mineglade.icore.events;
+
+import com.mineglade.icore.ICore;
+import com.mineglade.icore.PrefixType;
+import com.vexsoftware.votifier.model.Vote;
+import com.vexsoftware.votifier.model.VotifierEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class VoteEvent implements Listener {
+
+    @EventHandler
+    public void onPlayerVote(VotifierEvent event) {
+
+        Vote vote = event.getVote();
+
+        ICore.economy.bankDeposit(vote.getUsername(), 25);
+        Bukkit.broadcastMessage(ICore.getPrefix(PrefixType.COMMAND) + vote.getUsername() + " has voted on " + vote.getServiceName() + "!");
+
+    }
+
+}
