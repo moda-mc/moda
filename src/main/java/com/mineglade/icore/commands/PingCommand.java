0aeb743e91b10f918ee3962e88757432c6a3bf10
PatchPC <42698687+patchtv@users.noreply.github.com>
2019-06-15 17:01:05 +0200

    renamed to PingCommand

--- src/main/java/com/mineglade/icore/commands/PingCommand.java
+++ src/main/java/com/mineglade/icore/commands/PingCommand.java
@@ -1,6 +1,7 @@
 package com.mineglade.icore.commands;
 
 import com.mineglade.icore.ICore;
+import com.mineglade.icore.PrefixType;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -10,7 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import xyz.derkades.derkutils.bukkit.reflection.ReflectionUtil;
 
-public class Ping implements CommandExecutor {
+public class PingCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -26,7 +27,7 @@ public class Ping implements CommandExecutor {
         if (label.equalsIgnoreCase("hi")) {
             player.spigot().sendMessage(
                     new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getCommandPrefix()))
+                            .append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.COMMAND)))
                             .append("Hi there! (").color(ChatColor.GRAY)
                             .append(ping + "ms").color(ChatColor.GREEN)
                             .append(")").color(ChatColor.GRAY)
@@ -34,7 +35,7 @@ public class Ping implements CommandExecutor {
         } else {
             player.spigot().sendMessage(
                     new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getCommandPrefix()))
+                            .append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.COMMAND)))
                             .append("Your ping to the server is ").color(ChatColor.GRAY)
                             .append(ping + "ms").color(ChatColor.GREEN)
                             .create());
