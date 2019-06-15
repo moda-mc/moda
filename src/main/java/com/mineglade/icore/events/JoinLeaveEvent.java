8a2e54c6f4e68494c35fe7a5bac1e723163376c6
PatchPC <42698687+patchtv@users.noreply.github.com>
2019-06-15 17:04:23 +0200

    separated Join & Leave Event, added silentjoin & quit support, added motd

---
+++ src/main/java/com/mineglade/icore/events/JoinLeaveEvent.java
@@ -0,0 +1,61 @@
+package com.mineglade.icore.events;
+
+import com.mineglade.icore.ICore;
+import com.mineglade.icore.PrefixType;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import xyz.derkades.derkutils.bukkit.Chat;
+
+public class JoinLeaveEvent implements Listener {
+
+
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        event.setJoinMessage("");
+
+        final Player player = event.getPlayer();
+
+        if (ICore.isVanished(player)){
+            return;
+        }
+
+        FileConfiguration config = ICore.instance.getConfig();
+
+        Bukkit.spigot().broadcast(new ComponentBuilder("")
+                .append(ICore.getPrefix(PrefixType.JOIN))
+                .event((HoverEvent) null)
+                .append(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',ICore.chat.getPlayerPrefix(player) + " " )))
+                .append(player.getDisplayName()).color(ChatColor.GREEN)
+                .append(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&'," " + ICore.chat.getPlayerSuffix(player))))
+                .create());
+
+        player.spigot().sendMessage(Chat.toComponentWithPlaceholders(config.getList("motd"), player));
+
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        event.setQuitMessage("");
+
+        final Player player = event.getPlayer();
+
+        Bukkit.spigot()
+                .broadcast(new ComponentBuilder("")
+                        .append(ICore.getPrefix(PrefixType.LEAVE))
+                        .append(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',ICore.chat.getPlayerPrefix(player) + " " )))
+                        .append(player.getDisplayName()).color(ChatColor.RED)
+                        .append(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&'," " + ICore.chat.getPlayerSuffix(player))))
+                        .create());
+
+    }
+}
