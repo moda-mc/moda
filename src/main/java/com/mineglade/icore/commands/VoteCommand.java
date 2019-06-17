78040b3a36a44739e4353e75a46bfee7f56dc54a
PatchPC <42698687+patchtv@users.noreply.github.com>
2019-06-17 23:10:44 +0200

    now with voteCounts yay

--- src/main/java/com/mineglade/icore/commands/VoteCommand.java
+++ src/main/java/com/mineglade/icore/commands/VoteCommand.java
@@ -1,12 +1,19 @@
 package com.mineglade.icore.commands;
 
 import com.mineglade.icore.ICore;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import xyz.derkades.derkutils.bukkit.Chat;
+import xyz.derkades.derkutils.bukkit.PlaceholderUtil.Placeholder;
 
 public class VoteCommand implements CommandExecutor {
 
@@ -19,7 +26,33 @@ public class VoteCommand implements CommandExecutor {
             player.sendMessage("the /vote top command is not functional yet, please be patient.");
         }
         else {
-            player.spigot().sendMessage(Chat.toComponentWithPlaceholders(config.getList("vote-sites"), player));
+
+            
+            Bukkit.getScheduler().runTaskAsynchronously(ICore.instance, () -> {
+            	final int voteCount;
+            
+            	try {
+            		PreparedStatement statement = ICore.db.prepareStatement("SELECT `votes` FROM `votes` WHERE uuid=?", player.getUniqueId());
+					ResultSet result = statement.executeQuery(); 
+					
+					if (result.next()) {
+						voteCount = result.getInt("votes");
+					} 
+					else {
+						voteCount = 0;
+					}
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return;
+				}
+            
+            	Bukkit.getScheduler().runTask(ICore.instance, () -> {
+            		Placeholder votes = new Placeholder("%votes%", (p) -> voteCount + "");
+                    player.spigot().sendMessage(Chat.toComponentWithPlaceholders(config, "vote-response", player, votes));
+            	});
+            });
+            
         }
 
 
