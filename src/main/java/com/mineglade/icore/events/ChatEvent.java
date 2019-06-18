22694441463e34588913299a74ed73d8b18d7c9a
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-18 02:58:31 +0200

    in-game chat color added
    with permission icore.chat.color

--- src/main/java/com/mineglade/icore/events/ChatEvent.java
+++ src/main/java/com/mineglade/icore/events/ChatEvent.java
@@ -1,16 +1,18 @@
 package com.mineglade.icore.events;
 
-import com.mineglade.icore.ICore;
-import com.mineglade.icore.PrefixType;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import com.mineglade.icore.ICore;
+import com.mineglade.icore.PrefixType;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import xyz.derkades.derkutils.bukkit.Colors;
 
 public class ChatEvent implements Listener {
 
@@ -18,7 +20,10 @@ public class ChatEvent implements Listener {
     public void onChat(AsyncPlayerChatEvent event) {
 
         final Player player = event.getPlayer();
-
+        if (player.hasPermission("icore.chat.color")){
+        	  event.setMessage(Colors.parseColors(event.getMessage()));
+        }
+        
         for (Player recipient : event.getRecipients()) {
             recipient.spigot()
                     .sendMessage(new ComponentBuilder("")
