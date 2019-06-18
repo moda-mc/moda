4a571e07af19f6924be0d4872022107fc7bc3ecb
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-18 02:59:30 +0200

    VoteReminder added

---
+++ src/main/java/com/mineglade/icore/tasks/VoteReminder.java
@@ -0,0 +1,54 @@
+package com.mineglade.icore.tasks;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.mineglade.icore.ICore;
+
+import xyz.derkades.derkutils.bukkit.Chat;
+import xyz.derkades.derkutils.bukkit.PlaceholderUtil.Placeholder;
+
+public class VoteReminder extends BukkitRunnable {
+
+	@Override
+	public void run() {
+
+		FileConfiguration config = ICore.instance.getConfig();
+		for (Player player : Bukkit.getOnlinePlayers()) {
+
+			Bukkit.getScheduler().runTaskAsynchronously(ICore.instance, () -> {
+				final int voteCount;
+
+				try {
+					PreparedStatement statement = ICore.db.prepareStatement("SELECT `votes` FROM `votes` WHERE uuid=?",
+							player.getUniqueId());
+					ResultSet result = statement.executeQuery();
+
+					if (result.next()) {
+						voteCount = result.getInt("votes");
+					} else {
+						voteCount = 0;
+					}
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return;
+				}
+
+				Bukkit.getScheduler().runTask(ICore.instance, () -> {
+					Placeholder votes = new Placeholder("%votes%", voteCount + "");
+					player.spigot().sendMessage(
+							Chat.toComponentWithPapiPlaceholders(config, "voting.vote-reminder", player, votes));
+				});
+			});
+		}
+
+	}
+
+}
