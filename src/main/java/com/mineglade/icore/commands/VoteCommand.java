d6698800f7d04eff3fad63358caebef4d0da881d
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-20 01:52:06 +0200

    votecommand now has a response for if voting is disabled

--- src/main/java/com/mineglade/icore/commands/VoteCommand.java
+++ src/main/java/com/mineglade/icore/commands/VoteCommand.java
@@ -2,7 +2,7 @@ package com.mineglade.icore.commands;
 
 import com.mineglade.icore.ICore;
 
-//import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,12 +24,12 @@ public class VoteCommand implements CommandExecutor {
         Player player = (Player) sender;
         FileConfiguration config = ICore.instance.getConfig();
 
-//        if (!(ICore.instance.getConfig().getBoolean("mysql.enabled") && ICore.instance.getConfig().getBoolean("voting.enabled"))) {
-//        	player.spigot().sendMessage(new ComponentBuilder("")
-//        			.append("")
-//        			.create());
-//        	return true;
-//        }
+        if (!(ICore.instance.getConfig().getBoolean("mysql.enabled") && ICore.instance.getConfig().getBoolean("voting.enabled"))) {
+        	player.spigot().sendMessage(new ComponentBuilder("")
+        			.append("")
+        			.create());
+        	return true;
+        }
         if (args.length == 1 && args[0].equalsIgnoreCase("top")) {
             player.sendMessage("the /vote top command is not functional yet, please be patient.");
         }
