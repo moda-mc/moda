b7826d9924d0872d5b9dadeb869b754a1f31a90f
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-22 05:10:52 +0200

    started work on name and chat color commands

---
+++ src/main/java/com/mineglade/icore/commands/ColorCommand.java
@@ -0,0 +1,52 @@
+package com.mineglade.icore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.mineglade.icore.ICore;
+import com.mineglade.icore.PrefixType;
+import com.mineglade.icore.utils.CommandUtil;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+
+public class ColorCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {		
+		
+		if (args.length < 1) {
+			sender.sendMessage(ICore.getPrefix(PrefixType.COMMAND) + ChatColor.DARK_GRAY + "==== " + ChatColor.GREEN
+					+ "All iCore Commands" + ChatColor.DARK_GRAY + " ====");
+			CommandUtil.helpListEntry(sender, label, "name", "Allows you to set your name color", "icore.color.name", args);
+		}
+		
+		if (args[0] == "name") {
+
+			return true;
+		}
+		
+		else if (args[0] == "chat") {
+			if (!(sender instanceof Player)) {
+				
+			}
+			return true;
+		}
+		
+		else {
+			sender.spigot().sendMessage(new ComponentBuilder("")
+					.append("You need to specify a type of color")
+					.create());
+			return false;
+		}
+		
+	}
+	
+}
+
+
+
+// 
+// command: /color <chat|name> [player] <color>
