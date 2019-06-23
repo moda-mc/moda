a02985ca219f437e35b048ce59f4e2fb6ee96026
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-23 15:51:17 +0200

    you now only see the commands you have permission to.

--- src/main/java/com/mineglade/icore/utils/CommandUtil.java
+++ src/main/java/com/mineglade/icore/utils/CommandUtil.java
@@ -18,28 +18,31 @@ public class CommandUtil {
 
 	public static void helpListEntry(final CommandSender sender, final String label, final String subcommand,
 			final String description, final String permission, final String... aliases) {
-		sender.spigot().sendMessage(new ComponentBuilder("")
-				.append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.COMMAND))).event((HoverEvent) null)
-				.append("/" + label + " " + subcommand).color(ChatColor.GREEN)
-				.event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + label + " " + subcommand))
-				.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("")
-						.append("/" + label + " " + subcommand).bold(true).color(ChatColor.GREEN).append("\n")
-						.bold(false).color(ChatColor.RESET)
-						.append(TextComponent
-								.fromLegacyText(ChatColor.GREEN + "  Description: " + ChatColor.GRAY + description))
-						.append("\n").color(ChatColor.RESET)
-						.append(TextComponent.fromLegacyText(
-								ChatColor.GREEN + "  Usage: " + ChatColor.GRAY + "/" + label + " " + subcommand))
-						.append("\n").color(ChatColor.RESET)
-						.append(TextComponent.fromLegacyText(ChatColor.GREEN + "  Aliases: " + ChatColor.GRAY
-								+ String.join(", ",
-										Arrays.asList(aliases).stream().map((s) -> String.format("/%s %s", label, s))
-												.collect(Collectors.toSet()))))
-						.append("\n").color(ChatColor.RESET)
-						.append(TextComponent
-								.fromLegacyText(ChatColor.GREEN + "  Permission: " + ChatColor.WHITE + permission))
-						.create()))
-				.create());
+		if (sender.hasPermission(permission)) {
+			sender.spigot().sendMessage(new ComponentBuilder("")
+					.append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.COMMAND))).event((HoverEvent) null)
+					.append("/" + label + " " + subcommand).color(ChatColor.GREEN)
+					.event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + label + " " + subcommand))
+					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("")
+							.append("/" + label + " " + subcommand).bold(true).color(ChatColor.GREEN).append("\n")
+							.bold(false).color(ChatColor.RESET)
+							.append(TextComponent
+									.fromLegacyText(ChatColor.GREEN + "  Description: " + ChatColor.GRAY + description))
+							.append("\n").color(ChatColor.RESET)
+							.append(TextComponent.fromLegacyText(
+									ChatColor.GREEN + "  Usage: " + ChatColor.GRAY + "/" + label + " " + subcommand))
+							.append("\n").color(ChatColor.RESET)
+							.append(TextComponent.fromLegacyText(ChatColor.GREEN + "  Aliases: " + ChatColor.GRAY
+									+ String.join(", ",
+											Arrays.asList(aliases).stream()
+													.map((s) -> String.format("/%s %s", label, s))
+													.collect(Collectors.toSet()))))
+							.append("\n").color(ChatColor.RESET)
+							.append(TextComponent
+									.fromLegacyText(ChatColor.GREEN + "  Permission: " + ChatColor.WHITE + permission))
+							.create()))
+					.create());
+		}
 
 	}
 
