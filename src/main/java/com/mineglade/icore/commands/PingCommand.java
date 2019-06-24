735deb1bf08a6b54e4a8ecd661cbab0f9e2e2d38
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-24 23:22:24 +0200

    pingcommand rewrite
    now able to check another player's ping
    reads responses from messages.yml

--- src/main/java/com/mineglade/icore/commands/PingCommand.java
+++ src/main/java/com/mineglade/icore/commands/PingCommand.java
@@ -1,15 +1,19 @@
 package com.mineglade.icore.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.mineglade.icore.ICore;
+import com.mineglade.icore.utils.NickNameUtil;
 
-import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ClickEvent.Action;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import xyz.derkades.derkutils.bukkit.Colors;
 import xyz.derkades.derkutils.bukkit.reflection.ReflectionUtil;
 
 public class PingCommand implements CommandExecutor {
@@ -17,31 +21,67 @@ public class PingCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("You are the server. your ping is not applicable...");
-            return true;
-        }
-        final Player player = (Player) sender;
+    	Player player = (Player) sender;
+		if (!sender.hasPermission("icore.command.ping")) {
+			sender.spigot().sendMessage(new ComponentBuilder("")
+					.append(ICore.getPrefix())
+					.append(Colors.toComponent(ICore.messages.getString("errors.no-permission")
+							.replace("{command}", "/" + label)))
+					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("")
+							.append(Colors.toComponent(
+									"&7you need &aicore.command.ping&7 to set other people's nicknames")
+									)
+							.create()))
+					.create());
+		}
+		
+		if (args.length > 0) {
+			if (!sender.hasPermission("icore.command.ping.others")) {
+				sender.spigot().sendMessage(new ComponentBuilder("")
+						.append(ICore.getPrefix())
+						.append(Colors.toComponent(ICore.messages.getString("errors.no-permission")
+								.replace("{command}", "/" + label + " [target]")))
+						.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("")
+								.append(Colors.toComponent(
+										"&7you need &aicore.command.ping.others&7 to set other people's nicknames")
+										)
+								.create()))
+						.create());
+			}
+			Player target = Bukkit.getPlayer(args[0]);
+			if (target == null) {
+				sender.spigot().sendMessage(new ComponentBuilder("")
+						.append(ICore.getPrefix())
+						.append(Colors.toComponent(ICore.messages.getString("ping.errors.target-invalid")))
+						.event(new ClickEvent(Action.SUGGEST_COMMAND, "/" + label + " "))
+						.create());
+				return true;
+			}
+			String targetNickName = NickNameUtil.getNickName(target);
+			final int ping = ReflectionUtil.getPing(target);
+			player.spigot().sendMessage(new ComponentBuilder("")
+					.append(ICore.getPrefix())
+					.append(Colors.toComponent(ICore.messages.getString("ping.responses.others.ping")
+							.replace("{ping}", ping + "")
+							.replace("{target_nickname}", targetNickName)))
+					.create());
+			return true;
+		}
 
         final int ping = ReflectionUtil.getPing(player);
 
         if (label.equalsIgnoreCase("hi")) {
             player.spigot().sendMessage(
                     new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getPrefix()))
-                            .append("Hi there! (").color(ChatColor.GRAY)
-                            .append(ping + "ms").color(ChatColor.GREEN)
-                            .append(")").color(ChatColor.GRAY)
-                            .create());
-        } else {
-            player.spigot().sendMessage(
-                    new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getPrefix()))
-                            .append("Your ping to the server is ").color(ChatColor.GRAY)
-                            .append(ping + "ms").color(ChatColor.GREEN)
-                            .create());
-
-        }
+                    .append(ICore.getPrefix())
+                    .append(Colors.toComponent(ICore.messages.getString("ping.responses.self.hi").replace("{ping}", ping + "")))
+                    .create());
+            return true;
+		}
+		player.spigot().sendMessage(new ComponentBuilder("")
+				.append(ICore.getPrefix())
+				.append(Colors.toComponent(ICore.messages.getString("ping.responses.self.ping").replace("{ping}", ping + "")))
+				.create());
         return true;
     }
 }
