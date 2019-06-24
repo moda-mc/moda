cc2f7a5570dea990ac914f99c9efa5f5c61ab177
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-24 02:48:09 +0200

    prefixtype

--- src/main/java/com/mineglade/icore/commands/PingCommand.java
+++ src/main/java/com/mineglade/icore/commands/PingCommand.java
@@ -1,14 +1,15 @@
 package com.mineglade.icore.commands;
 
-import com.mineglade.icore.ICore;
-import com.mineglade.icore.PrefixType;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import com.mineglade.icore.ICore;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import xyz.derkades.derkutils.bukkit.reflection.ReflectionUtil;
 
 public class PingCommand implements CommandExecutor {
@@ -27,7 +28,7 @@ public class PingCommand implements CommandExecutor {
         if (label.equalsIgnoreCase("hi")) {
             player.spigot().sendMessage(
                     new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.PLUGIN)))
+                            .append(TextComponent.fromLegacyText(ICore.getPrefix()))
                             .append("Hi there! (").color(ChatColor.GRAY)
                             .append(ping + "ms").color(ChatColor.GREEN)
                             .append(")").color(ChatColor.GRAY)
@@ -35,7 +36,7 @@ public class PingCommand implements CommandExecutor {
         } else {
             player.spigot().sendMessage(
                     new ComponentBuilder("")
-                            .append(TextComponent.fromLegacyText(ICore.getPrefix(PrefixType.PLUGIN)))
+                            .append(TextComponent.fromLegacyText(ICore.getPrefix()))
                             .append("Your ping to the server is ").color(ChatColor.GRAY)
                             .append(ping + "ms").color(ChatColor.GREEN)
                             .create());
