59c6c9ec64a837027ad99d723ef32b0125819c28
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-30 00:13:40 +0200

    Update ColorPickerMenu.java
    ColorPickerMenu now where you can pick the color you wish to set

--- src/main/java/com/mineglade/icore/chat/colors/ColorPickerMenu.java
+++ src/main/java/com/mineglade/icore/chat/colors/ColorPickerMenu.java
@@ -1,59 +1,104 @@
 package com.mineglade.icore.chat.colors;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import com.mineglade.icore.ICore;
+import com.mineglade.icore.utils.PlayerData;
 
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import xyz.derkades.derkutils.bukkit.Colors;
 import xyz.derkades.derkutils.bukkit.ItemBuilder;
 import xyz.derkades.derkutils.bukkit.menu.IconMenu;
 import xyz.derkades.derkutils.bukkit.menu.OptionClickEvent;
 
-public class ColorPickerMenu extends IconMenu {
-	
-	private Player target;
+public class ColorPickerMenu extends IconMenu{
 
-	public ColorPickerMenu(Player player, Player target) {
-		super(ICore.instance, "Color Picker", 9, player);
+	private Player target;
+	private Player player;
+	private IColorType type;
+	
+	public ColorPickerMenu(Player player, Player target, IColorType type) {
+		super(ICore.instance, StringUtils.capitalize(type.toString().toLowerCase()) + " Colors", 3*9, player);
 		this.target = target;
-		if (player.hasPermission("icore.command.color.name.others") 
-				|| (player.equals(target) && player.hasPermission("icore.command.color.name"))) {
-			items.put(2, new ItemBuilder(Material.NAME_TAG)
-					.coloredName("&6Name Color")
-					.coloredLore("&e&oSet your name color!")
-					.create());
-		} else {
-			items.put(2, new ItemBuilder(Material.BARRIER)
-					.coloredName("&cName Color")
-					.coloredLore("&c&oYou need &ficore.command.color.name\n&cto set your name color.")
-					.create());
+		this.player = player;
+		this.type = type;
+		items.put(0, new ItemBuilder(Material.WATER_BUCKET)
+				.name("Reset")
+				.coloredLore("&7Reset your name color.")
+				.damage(15)
+				.create());
+		int i = 1;
+		for (IColor iColor : IColor.values()) {
+			if (player.hasPermission(iColor.getPermission(type))) {
+				items.put(i, iColor.getItem(type));
+			} else {
+				items.put(i, new ItemBuilder(Material.BARRIER)
+						.coloredName("&c" + iColor.getName())
+						.coloredLore("&7You need &a" + iColor.getPermission(type) + "&7 to set this color.")
+						.create());
+			}
+			i++;
 		}
-		
-		if (player.hasPermission("icore.command.color.chat.others") 
-				|| (player.equals(target) && player.hasPermission("icore.command.color.chat"))) {
-			items.put(6, new ItemBuilder(Material.BOOK_AND_QUILL)
-					.coloredName("&6Chat Color")
-					.coloredLore("&e&oSet your chat color!")
-					.create());
-		} else {
-			items.put(6, new ItemBuilder(Material.BOOK_AND_QUILL)
-					.coloredName("&cChat Color")
-					.coloredLore("&c&oYou need &ficore.command.color.chat\n&cto set your chat color.")
-					.create());
-		}
+		items.put(26, new ItemBuilder(Material.BARRIER)
+				.name("Back")
+				.coloredLore("&7Go back to the previous menu.")
+				.create());
 	}
 
 	@Override
 	public boolean onOptionClick(OptionClickEvent event) {
-		if (event.getPosition() == 2 && !event.getItemStack().getType().equals(Material.BARRIER)) {
-			new NameColorPickerMenu(event.getPlayer(), target).open();
+		PlayerData data = new PlayerData(target);
+		if (type == IColorType.NAME) {
+			if (event.getName().contains("Reset")) {
+				data.resetNameColor();
+				player.sendMessage(ICore.getPrefix() + Colors.parseColors(ICore.messages.getString("color.name.color.reset")));
+				return true;
+			} else if (event.getName().contains("Back")) {
+				new ColorMenu(player, target).open();
+				return false;
+			} else {
+				IColor color = IColor.fromName(event.getName());
+				if (!player.hasPermission(color.getPermission(IColorType.NAME))) {
+					player.spigot().sendMessage(new ComponentBuilder("")
+							.append(ICore.getPrefix())
+							.append(Colors.toComponent(ICore.messages.getString("color.errors.no-permission")
+									.replace("{color}", "&" + color.getColorCode() + color.getName())))
+							.create());
+					return true;
+				}
+				data.setNameColor(color.getColorCode());
+				player.sendMessage(ICore.getPrefix() + Colors.parseColors(ICore.messages.getString("color.name.color.set")
+						.replace("{color}", "&" + color.getColorCode() + color.getName())));
+				return true;
+			}
 		}
-		if (event.getPosition() == 6 && !event.getItemStack().getType().equals(Material.BARRIER)) {
-			
+		if (type == IColorType.CHAT) {
+			if (event.getName().contains("Reset")) {
+				data.resetChatColor();
+				player.sendMessage(ICore.getPrefix() + Colors.parseColors(ICore.messages.getString("color.chat.color.reset")));
+				return true;
+			} else if (event.getName().contains("Back")) {
+				new ColorMenu(player, target).open();
+				return false;
+			} else {
+				IColor color = IColor.fromName(event.getName());
+				if (!player.hasPermission(color.getPermission(IColorType.NAME))) {
+					player.spigot().sendMessage(new ComponentBuilder("")
+							.append(ICore.getPrefix())
+							.append(Colors.toComponent(ICore.messages.getString("color.errors.no-permission")
+									.replace("{color}", "&" + color.getColorCode() + color.getName())))
+							.create());
+					return true;
+				}
+				data.setChatColor(color.getColorCode());
+				player.sendMessage(ICore.getPrefix() + Colors.parseColors(ICore.messages.getString("color.chat.color.set")
+						.replace("{color}", "&" + color.getColorCode() + color.getName())));
+				return true;
+		
+			}
 		}
 		return false;
 	}
-	
-	
-	
 }
