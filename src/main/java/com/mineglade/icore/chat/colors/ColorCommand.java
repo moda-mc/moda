58210e0758462c603186f4f8448c7315950e402d
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-30 00:12:35 +0200

    Update ColorCommand.java
    color command now opens a gui to select the type of color you wish to update

--- src/main/java/com/mineglade/icore/chat/colors/ColorCommand.java
+++ src/main/java/com/mineglade/icore/chat/colors/ColorCommand.java
@@ -1,9 +1,17 @@
 package com.mineglade.icore.chat.colors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import com.mineglade.icore.ICore;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.ClickEvent.Action;
+import xyz.derkades.derkutils.bukkit.Colors;
 
 public class ColorCommand implements CommandExecutor {
 
@@ -13,7 +21,18 @@ public class ColorCommand implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (args.length < 1) {
-				new ColorPickerMenu(player, player).open();
+				new ColorMenu(player, player).open();
+			} else {
+				Player target = Bukkit.getPlayer(args[0]);
+				if (target == null) {
+					sender.spigot().sendMessage(new ComponentBuilder("")
+							.append(ICore.getPrefix())
+							.append(Colors.toComponent(ICore.messages.getString("color.errors.target-invalid")))
+							.event(new ClickEvent(Action.SUGGEST_COMMAND, "/" + label + " "))
+							.create());
+					return true;
+				}
+				new ColorMenu(player, target).open();
 			}
 		} else {
 			sender.sendMessage("this command is not functional for the console yet.");
