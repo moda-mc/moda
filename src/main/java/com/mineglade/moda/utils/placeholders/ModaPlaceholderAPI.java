a708a22f48a88150eca28d2102772b0d8c49c0c1
Robin <robinslot@hotmail.nl>
2019-07-08 18:08:02 +0200

    New parse placeholders method

--- src/main/java/com/mineglade/moda/utils/placeholders/ModaPlaceholderAPI.java
+++ src/main/java/com/mineglade/moda/utils/placeholders/ModaPlaceholderAPI.java
@@ -6,6 +6,9 @@ import java.util.function.Function;
 
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+
 public class ModaPlaceholderAPI {
 
 	private static final char PLACEHOLDER_START = '{';
@@ -28,6 +31,26 @@ public class ModaPlaceholderAPI {
 
 	public static void addPlaceholder(final String placeholder, final Function<Player, Object> supplier) {
 		PLACEHOLDERS.put(placeholder, supplier);
+	}
+
+
+	public static BaseComponent[] parsePlaceholders(final Player player, final BaseComponent... components) {
+		final BaseComponent[] newComponents = new BaseComponent[components.length];
+		for (int i = 0; i < components.length; i++) {
+			final BaseComponent originalComponent = components[i];
+			for (final Map.Entry<String, Function<Player, Object>> placeholderEntry : PLACEHOLDERS.entrySet()) {
+				final String text = originalComponent.toPlainText();
+				if (text.contains(formatPlaceholder(placeholderEntry.getKey()))) {
+					final String newText = text.replace(formatPlaceholder(placeholderEntry.getKey()), placeholderEntry.getValue().apply(player).toString());
+					final BaseComponent component = new TextComponent(newText);
+					component.copyFormatting(originalComponent);
+					newComponents[i] = component;
+				} else {
+					newComponents[i] = components[i];
+				}
+			}
+		}
+		return newComponents;
 	}
 
 }
