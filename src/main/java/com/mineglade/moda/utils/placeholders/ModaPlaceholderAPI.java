cfc84cd67a6e276a3c921f19773fa8ce71207be7
Robin <robinslot@hotmail.nl>
2019-07-08 03:32:54 +0200

    started working on placeholder api

---
+++ src/main/java/com/mineglade/moda/utils/placeholders/ModaPlaceholderAPI.java
@@ -0,0 +1,33 @@
+package com.mineglade.moda.utils.placeholders;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.bukkit.entity.Player;
+
+public class ModaPlaceholderAPI {
+
+	private static final char PLACEHOLDER_START = '{';
+	private static final char PLACEHOLDER_END = '}';
+
+	private static final Map<String, Function<Player, Object>> PLACEHOLDERS = new HashMap<>();
+
+	public static String parsePlaceholders(String string, final Player player) {
+		for (final Map.Entry<String, Function<Player, Object>> placeholderEntry : PLACEHOLDERS.entrySet()) {
+			if (string.contains(formatPlaceholder(placeholderEntry.getKey()))) {
+				string = string.replace(formatPlaceholder(placeholderEntry.getKey()), placeholderEntry.getValue().get(player).toString());
+			}
+		}
+		return string;
+	}
+
+	private static String formatPlaceholder(final String placeholder) {
+		return PLACEHOLDER_START + placeholder + PLACEHOLDER_END;
+	}
+
+	public static void addPlaceholder(final String placeholder, final Function<Player, Object> supplier) {
+		PLACEHOLDERS.put(placeholder, supplier);
+	}
+
+}
