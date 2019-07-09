d54b571b1ebd1bd9e403335ce595fe12b3884bb3
Robin <42698687+patchtv@users.noreply.github.com>
2019-07-09 19:53:32 +0200

    update ChatModule

--- src/main/java/com/mineglade/moda/modules/chat/ChatModule.java
+++ src/main/java/com/mineglade/moda/modules/chat/ChatModule.java
@@ -8,13 +8,19 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import com.mineglade.moda.modules.IMessage;
 import com.mineglade.moda.modules.Module;
 import com.mineglade.moda.modules.chat.storage.ChatStorageHandler;
+import com.mineglade.moda.utils.placeholders.ModaPlaceholderAPI;
 import com.mineglade.moda.utils.storage.DatabaseStorageHandler;
 import com.mineglade.moda.utils.storage.FileStorageHandler;
+import com.mineglade.moda.utils.vault.VaultHandler;
+import com.mineglade.moda.utils.vault.VaultNotAvailableException;
 
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import xyz.derkades.derkutils.bukkit.Chat;
 import xyz.derkades.derkutils.bukkit.Colors;
+import xyz.derkades.derkutils.bukkit.PlaceholderUtil.Placeholder;
 
 public class ChatModule extends Module<ChatStorageHandler> {
+
+	private VaultHandler vault;
 
 	@Override
 	public String getName() {
@@ -36,21 +42,36 @@ public class ChatModule extends Module<ChatStorageHandler> {
 		return null;
 	}
 
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-	public void onChat(AsyncPlayerChatEvent event) {
+	@Override
+	public void onEnable() {
+		try {
+			this.vault = new VaultHandler(this);
+			this.vault.getChat();
+		} catch (final VaultNotAvailableException e) {
+			this.logger.info("Vault not installed, vault Placeholders will not work.");
+			this.vault = null;
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+	public void onChat(final AsyncPlayerChatEvent event) {
 		final Player player = event.getPlayer();
 
 		if (player.hasPermission("moda.chat.use_colors")) {
 			event.setMessage(Colors.parseColors(event.getMessage()));
 		}
-		/*
-		 * for (Placeholder placeholder : EmotePlaceholders.parseEmote(player)) {
-		 * event.setMessage(placeholder.parse(event.getMessage())); }
-		 */
 
-		for (Player recipient : event.getRecipients()) {
-			recipient.spigot()
-					.sendMessage(new ComponentBuilder(event.getMessage()).create());
+		final Placeholder message = new Placeholder("{MESSAGE}", event.getMessage());
+
+		final Placeholder vaultPrefix = new Placeholder("{PREFIX}", this.vault == null ? "&c[NoVault]" : this.vault.getChat().getPlayerPrefix(player));
+		final Placeholder vaultSuffix = new Placeholder("{SUFFIX}", this.vault == null ? "&c[NoVault] " : this.vault.getChat().getPlayerSuffix(player));
+		final Placeholder vaultGroup = new Placeholder("{GROUP}", this.vault == null ? "&c[NoVault]" : this.vault.getChat().getPrimaryGroup(player));
+
+		for (final Player recipient : event.getRecipients()) {
+			recipient.spigot().sendMessage(
+					ModaPlaceholderAPI.parsePlaceholders(player,
+							Chat.toComponentWithPapiPlaceholders(this.config, "format", player,
+									message, vaultPrefix, vaultSuffix, vaultGroup)));
 		}
 		event.getRecipients().clear();
 	}
