e2211b8077f8442f36677cb7d187af2487ebbb41
Derkades <robinslot@hotmail.nl>
2020-04-06 00:22:44 +0200

    Register listener in module

--- src/moda/plugin/moda/util/IconMenu.java
+++ src/moda/plugin/moda/util/IconMenu.java
@@ -8,7 +8,7 @@ import moda.plugin.moda.module.Module;
 public abstract class IconMenu extends xyz.derkades.derkutils.bukkit.menu.IconMenu {
 
 	public IconMenu(final Module<?> module, final String name, final int size, final Player player) {
-		super(Moda.instance, name, size, player); // TODO Somehow modify Derkutils so it is possible to register the event from a module
+		super(Moda.instance, name, size, player, l -> module.registerListener(l));
 	}
 
 }
