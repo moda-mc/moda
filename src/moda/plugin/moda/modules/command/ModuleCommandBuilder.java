fdc12b215e2482cfb5baec805cae78acae98af06
Derkades <robinslot@hotmail.nl>
2020-04-05 22:06:30 +0200

    Create ModuleCommandBuilder.java

---
+++ src/moda/plugin/moda/modules/command/ModuleCommandBuilder.java
@@ -0,0 +1,80 @@
+package moda.plugin.moda.modules.command;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.Plugin;
+
+import moda.plugin.moda.Moda;
+import moda.plugin.moda.modules.Module;
+import moda.plugin.moda.utils.storage.ModuleStorageHandler;
+
+public class ModuleCommandBuilder {
+	
+	private final PluginCommand command;
+	
+	public ModuleCommandBuilder(final String name) {
+		try {
+			final Constructor<PluginCommand> constructor = PluginCommand.class.getConstructor(String.class, Plugin.class);
+			constructor.setAccessible(true);
+			this.command = constructor.newInstance(name, Moda.instance);
+		} catch (final InvocationTargetException | InstantiationException |
+				IllegalAccessException | IllegalArgumentException | NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public ModuleCommandBuilder withPermission(final String permission) {
+		this.command.setPermission(permission);
+		return this;
+	}
+	
+	public ModuleCommandBuilder withAlias(final String alias) {
+		this.command.getAliases().add(alias);
+		return this;
+	}
+	
+	public ModuleCommandBuilder withAliases(final String... alias) {
+		this.command.getAliases().addAll(Arrays.asList(alias));
+		return this;
+	}
+	
+	public ModuleCommandBuilder withAliases(final Collection<String> aliases) {
+		this.command.getAliases().addAll(aliases);
+		return this;
+	}
+	
+	public ModuleCommandBuilder withUsage(final String usage) {
+		this.command.setUsage(usage);
+		return this;
+	}
+	
+	public ModuleCommandBuilder withDescription(final String description) {
+		this.command.setDescription(description);
+		return this;
+	}
+
+	public ModuleCommandBuilder withExecutor(final CommandExecutor executor) {
+		this.command.setExecutor(executor);
+		return this;
+	}
+	
+	public ModuleCommandBuilder withTabCompleter(final TabCompleter tabCompleter) {
+		this.command.setTabCompleter(tabCompleter);
+		return this;
+	}
+	
+	public Command create() {
+		return this.command;
+	}
+	
+	public void register(final Module<? extends ModuleStorageHandler> module) {
+		module.registerCommand(this.command);
+	}
+}
