3ef9e4e41462d33dcde0e2d05814b72c575ca71b
Robin <robinslot@hotmail.nl>
2021-03-01 11:55:22 +0100

    update bstats

--- src/cx/moda/moda/Stats.java
+++ src/cx/moda/moda/Stats.java
@@ -3,14 +3,17 @@ package cx.moda.moda;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.AdvancedPie;
+import org.bstats.charts.SimplePie;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import cx.moda.moda.module.Module;
 import cx.moda.moda.module.ModuleManager;
-import org.bstats.bukkit.Metrics;
-import org.bukkit.plugin.Plugin;
 
 public class Stats extends Metrics {
 
-	public Stats(final Plugin plugin) {
+	public Stats(final JavaPlugin plugin) {
 		super(plugin, 5106);
 
 		this.addCustomChart(new SimplePie("storage_type", () -> Moda.instance.getStorageType().name().toString()));
